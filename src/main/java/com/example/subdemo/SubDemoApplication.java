6ee1fbf9cb67f7ebbb893800a71ddf12d2271710
lvsheng <sheng.lv@oceanus-plus.com>
2021-12-09 17:03:18 +0800

    first commit

---
+++ src/main/java/com/example/subdemo/SubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.subdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SubDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/subdemo/SubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
